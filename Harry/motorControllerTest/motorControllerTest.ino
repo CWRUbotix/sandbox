a67af5a39da605364e2d0ed97895c1131f2077e8
Chromatk <harrykwon20@gmail.com>
2015-09-22 10:40:05 -0400

    created motorControllerTest

---
+++ Harry/motorControllerTest/motorControllerTest.ino
@@ -0,0 +1,12 @@
+//motor controller code
+
+motorOneAddress = 128;
+
+void setup() {
+  Serial.begin(9600); //pc connection
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
